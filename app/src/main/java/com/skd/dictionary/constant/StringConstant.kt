016477b641188d93155a0ad905f54a42f6dd2fa3
saruj <14skd12@gmail.com>
2026-01-18 22:58:24 +0530

    created string constant file

---
+++ app/src/main/java/com/skd/dictionary/constant/StringConstant.kt
@@ -0,0 +1,21 @@
+package com.skd.dictionary.constant
+
+object StringConstant {
+    const val INDIA = "IN"
+    const val HINDI = "Hindi"
+    const val SHORT_HINDI = "hi"
+    const val TAMIL = "Tamil"
+    const val SHORT_TAMIL = "ta"
+    const val TELUGU = "Telugu"
+    const val SHORT_TELUGU = "te"
+    const val KANNADA = "Kannada"
+    const val SHORT_KANNADA = "kn"
+    const val BENGALI = "Bengali"
+    const val SHORT_BENGALI = "kn"
+    const val MARATHI = "Marathi"
+    const val SHORT_MARATHI = "mr"
+    const val GUJARATI = "Gujarati"
+    const val SHORT_GUJARATI = "gu"
+    const val URDU = "Urdu"
+    const val SHORT_URDU = "ur"
+}
