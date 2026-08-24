ac5cc9137e79f524b15ef052ab62f3042a130bf9
saruj <14skd12@gmail.com>
2026-08-24 22:01:59 +0530

    gradle upgrade with version 5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.saruj.dictionary"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.4.0"
+        versionCode = 5
+        versionName = "1.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
