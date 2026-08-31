d09b8497948f756bc6ba4b49afc7f69fd10bf60f
saruj <14skd12@gmail.com>
2026-08-31 20:21:18 +0530

    gradle upgrade with version 5 along with version name

--- app/src/main/java/com/skd/dictionary/activity/DictionaryActivity.kt
+++ app/src/main/java/com/skd/dictionary/activity/DictionaryActivity.kt
@@ -19,7 +19,6 @@ import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
-
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.ViewModelProvider
