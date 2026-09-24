e4bec770a7872fb03dfb2a159a645db442134f4a
saruj <14skd12@gmail.com>
2026-09-24 21:38:50 +0530

    gradle upgrade with version 5 along with version name

--- app/src/main/java/com/skd/dictionary/activity/DictionaryActivity.kt
+++ app/src/main/java/com/skd/dictionary/activity/DictionaryActivity.kt
@@ -200,6 +200,8 @@ class DictionaryActivity : AppCompatActivity() {
         tvPronunciation = findViewById(R.id.tvPronunciation)
         progressTranslation = findViewById(R.id.progressTranslation)
         rvWordDetails.layoutManager = LinearLayoutManager(this)
+
+        
         val ivInfoLogo: ImageView = findViewById(R.id.ivInfoLogo)
 
         wordDetailAdapter = WordDetailAdapter(mutableListOf())
